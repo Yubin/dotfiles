d32aeb6d4e0aaf0b581a39967d6575233d0ce9c1
Ming Liu <vmliu1@gmail.com>
2015-11-08 19:38:43 -0800

    sync fish config

---
+++ fish/config.fish
@@ -0,0 +1,19 @@
+
+# Raise file descriptor limit
+ulimit -n 32768
+
+fish_vi_mode
+
+source ~/.config/fish/nvm.fish
+
+# Alias
+alias vim=nvim
+alias ll=ls -l
+
+function fish_prompt
+  env FISH_VERSION=$FISH_VERSION PROMPTLINE_LAST_EXIT_CODE=$status bash ~/.config/nvim/promptline.sh left
+end
+
+function fish_right_prompt
+  env FISH_VERSION=$FISH_VERSION PROMPTLINE_LAST_EXIT_CODE=$status bash ~/.config/nvim/promptline.sh right
+end
