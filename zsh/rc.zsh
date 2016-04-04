bbaa1b97fe925100eb0b4563039255be6ecf372f
Ming Liu <vmliu1@gmail.com>
2016-04-03 19:52:41 -0700

    export zsh path

--- zsh/rc.zsh
+++ zsh/rc.zsh
@@ -7,3 +7,10 @@ source ~/.dotfiles/shell/promptline.sh
 alias vim="nvim"
 alias vi="nvim"
 alias ll="ls -l"
+
+export PATH=~/.dotfiles/bin:$PATH
+
+for ruby in `find ~/.gem/ruby -type d -maxdepth 2 | grep 'bin$'`
+do
+  export PATH=$ruby:$PATH
+done
