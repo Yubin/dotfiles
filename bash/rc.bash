11099a1526cf3ee1f10c378890af105f0ddb77a5
Ming Liu <vmliu1@gmail.com>
2016-04-11 19:42:29 -0700

    source bash-it

--- bash/rc.bash
+++ bash/rc.bash
@@ -1,5 +1,10 @@
 
+DOTFILES=~/.dotfiles
+BASH_IT=$DOTFILES/bash/bash-it
+
 # Raise file descriptor limit
 ulimit -n 8192
 
-source ~/.dotfiles/shell/promptline.sh
+source $DOTFILES/shell/promptline.sh
+
+source $BASH_IT/bash_it.sh
